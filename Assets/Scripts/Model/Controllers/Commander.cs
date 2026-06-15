5cb8a7e2877f83f5143687b6ce7e4a294429974a
Yaroslav Volkov <yvi_7693@icloud.com>
2026-06-15 10:24:53 +0300

    feat: добавлен класс PlansOfficer

--- Assets/Scripts/Model/Controllers/Commander.cs
+++ Assets/Scripts/Model/Controllers/Commander.cs
@@ -4,6 +4,6 @@ public class Commander
 
     public bool TryAssignMission()
     {
-        return true;
+        
     }
 }
--- Assets/Scripts/Model/Controllers/PlansOfficer.cs
+++ Assets/Scripts/Model/Controllers/PlansOfficer.cs
@@ -1,4 +1,16 @@
+using System.Collections.Generic;
+
 class PlansOfficer
 {
-    
+    private TurnRecon turnRecon;
+
+    public (bool, Sector) PlanOrder(int targetX, int targetY)
+    {
+        
+    }
+
+    public bool TryDeployShip(Ship ship, List <(int x, int y)> positions)
+    {
+        
+    }
 }
