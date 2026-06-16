9e3a6fec865571c501e690ad5825c0b67303556f
Yaroslav Volkov <yvi_7693@icloud.com>
2026-06-16 17:31:51 +0300

    feat: добавлен метод ReconOrder

--- Assets/Scripts/Model/Controllers/TurnRecon.cs
+++ Assets/Scripts/Model/Controllers/TurnRecon.cs
@@ -6,16 +6,26 @@ public class TurnRecon
     private Sea sea2;
     private Sea queue;
 
-    public TurnRecon(Sea sea1, Sea sea2)
+    public TurnRecon(Sea sea1, Sea sea2, Sea queue)
     {
         this.sea1 = sea1;
         this.sea2 = sea2;
 
-        queue = null;
+        this.queue = queue;
     }
 
     public Sea ReconOrder()
     {
-        
+        if (queue == sea1)
+        {
+            queue = sea2;
+            return sea1;
+        }
+
+        else
+        {
+            queue = sea1;
+            return sea2;
+        }
     }
 }
