15bc1502707c8b2fe0f3b6e3278f6d3655d2a0bb
Yaroslav Volkov <yvi_7693@icloud.com>
2026-07-04 21:51:28 +0300

    feat: добавлены анимации кнопок

---
+++ Assets/Scripts/View/ButtonTween.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class ButtonTween : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private float hoverScale = 1.08f;
+    [SerializeField] private float duration = 0.12f;
+    private Vector3 defoultScale;
+
+    private void Awake()
+    {
+        defoultScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        transform.DOScale(defoultScale * hoverScale, duration).SetEase(Ease.OutBack);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        transform.DOScale(defoultScale, duration).SetEase(Ease.OutQuad);
+    }
+}
