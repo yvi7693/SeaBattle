9344e71f11d237cc9f376d5a5be0e9d3b88474c0
Yaroslav Volkov <yvi_7693@icloud.com>
2026-07-06 22:00:31 +0300

    feat: добавлена пульсирующая анимация текста

---
+++ Assets/Scripts/View/PulseAnimate.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class PulseAnimate : MonoBehaviour
+{
+    [SerializeField] private TMP_Text animateText;
+    [SerializeField] private float duration = 1.2f;
+    private Sequence sequence;
+
+    [Header("Colors")]
+
+    [SerializeField] private Color brightColor = Color.white;
+    [SerializeField] private Color dimColor = new Color(0.78f, 0.9f, 1f, 0.78f);
+
+    public void Start()
+    {
+        OnAnimation();
+    }
+
+    public void OnAnimation()
+    {
+         sequence = DOTween.Sequence()
+            .Append(animateText.DOColor(dimColor, duration))
+            .Append(animateText.DOColor(brightColor, duration))
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1);
+    }
+
+
+
+}
