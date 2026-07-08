8cd4222a212aea43d6fba82f6e09ecb46816080c
Yaroslav Volkov <yvi_7693@icloud.com>
2026-07-08 08:13:02 +0300

    feat: добавлена анимация сцены победы

---
+++ Assets/Scripts/View/WinnerAnimaation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class WinnerAnimation : MonoBehaviour
+{
+    [SerializeField] private TMP_Text winnerText;
+    [SerializeField] private TMP_Text winnerName;
+
+    private void Start()
+    {
+        winnerText.alpha = 0;
+        winnerName.alpha = 0;
+
+        DOTween.Sequence()
+            .Append(winnerText.DOFade(1f, 1f))
+            .SetDelay(1f)
+            .Append(winnerName.DOFade(1f, 2f));
+    }
+}
