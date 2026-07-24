ec49b0ef0d578445e22f37fb6bae3db180507816
Yaroslav Volkov <yvi_7693@icloud.com>
2026-07-24 19:57:05 +0300

    feat: добавлен интерфейс количества активных кораблей

--- Assets/Scripts/View/Restart.cs
+++ Assets/Scripts/View/Restart.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,7 +9,9 @@ public class Restart : MonoBehaviour
 
     public void NewGame()
     {
+        DOTween.KillAll();
         Destroy(GameSession.Instance.gameObject);
+        
         SceneManager.LoadScene("StartScene");
 
         MusicPlayer.Instance.Switch(startMusic);
