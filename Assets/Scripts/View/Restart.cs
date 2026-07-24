cb0700a748d0abf17b4b5323880d484e6d34478d
Yaroslav Volkov <yvi_7693@icloud.com>
2026-07-24 23:19:57 +0300

    feat: реализована кнопка back

--- Assets/Scripts/View/Restart.cs
+++ Assets/Scripts/View/Restart.cs
@@ -4,16 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    [SerializeField] private AudioSource startMusic;
-
-
     public void NewGame()
     {
         DOTween.KillAll();
         Destroy(GameSession.Instance.gameObject);
-        
+        Destroy(MusicPlayer.Instance.gameObject);
+
         SceneManager.LoadScene("StartScene");
 
-        MusicPlayer.Instance.Switch(startMusic);
+       
     }
 }
