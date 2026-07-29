f1f804b84eaf6db48f58c4459a61e293d0470c6b
Yaroslav Volkov <yvi_7693@icloud.com>
2026-07-29 15:22:19 +0300

    feat: добавлена анимация появления персонажей

--- Assets/Scripts/View/CommentaryView.cs
+++ Assets/Scripts/View/CommentaryView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class CommentaryView : MonoBehaviour
 {
@@ -18,25 +19,42 @@ public class CommentaryView : MonoBehaviour
     [SerializeField] private TMP_Text textLeft;
     [SerializeField] private TMP_Text textRight;
 
+    public void Awake()
+    {
+        leftGroup.alpha = 0;
+        rightGroup.alpha = 0;
+    }
+
 
     public void ShowLeftPerson(bool mood)
     {
         imageLeft.sprite = personLeft.GetIcon();
         textLeft.text = RandomMessage(personLeft, mood);
 
-        rightGroup.gameObject.SetActive(false);
-        leftGroup.gameObject.SetActive(true);
+        Animate(leftGroup);
     }
 
 
     public void ShowRightPerson(bool mood)
     {
-        
         imageRight.sprite = personRight.GetIcon();
         textRight.text = RandomMessage(personRight, mood);
 
-        leftGroup.gameObject.SetActive(false);
-        rightGroup.gameObject.SetActive(true);
+        Animate(rightGroup);
+    }
+
+
+    public void Animate(CanvasGroup group)
+    {
+        group.gameObject.SetActive(true);
+
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(group.DOFade(1f, 0.5f).SetEase(Ease.InOutSine));
+        sequence.AppendInterval(1f);
+        sequence.Append(group.DOFade(0f, 0.5f).SetEase(Ease.InOutSine));
+        sequence.OnComplete(() =>
+            group.gameObject.SetActive(false));
+
     }
 
     public string RandomMessage(Person person, bool positive)
